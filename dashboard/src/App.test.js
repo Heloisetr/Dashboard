6b2b294dc98f0012a4b3201720fcfd9ff667b4c3
TRONCHE <heloise3.tronche@epitech.eu>
2019-11-02 21:31:18 +0100

    Fix the problem with GitKraken when the merge happend.
    Recode the login and signup and footer and header and change the path in the index.js and Apptest.js of the component App

--- dashboard/src/App.test.js
+++ dashboard/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppLogIn from './Authentification/LogIn';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AppLogIn />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

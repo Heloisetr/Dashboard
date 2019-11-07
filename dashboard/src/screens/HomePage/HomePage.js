135e63acbb20be1b09945e0829d180cb3970cfc6
TRONCHE <heloise3.tronche@epitech.eu>
2019-11-07 16:19:29 +0100

    Push the file for the HomePage

---
+++ dashboard/src/screens/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import './HomePage.css'
+
+export default withRouter(function HomePage({history}) {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>  
+    );
+})

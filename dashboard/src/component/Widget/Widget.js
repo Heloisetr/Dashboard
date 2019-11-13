5bbb35c441797860883d0d2a2638900f09bd9488
TRONCHE <heloise3.tronche@epitech.eu>
2019-11-13 14:57:09 +0100

    Push Widget.js
    Adding a new Modal use to Configure the widget.

--- dashboard/src/component/Widget/Widget.js
+++ dashboard/src/component/Widget/Widget.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Button, Popover, OverlayTrigger, Badge} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Button, Popover, OverlayTrigger, Badge, Modal} from 'react-bootstrap';
 
 import './Widget.css'
 
@@ -11,6 +11,11 @@ const images = {
 export default function Widget(props)
 {
     
+    const [show, setShow] = useState(false);
+    const [name, setName] = useState("");
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const popinfos = (
         <Popover id="Widget-popover">
@@ -31,7 +36,21 @@ export default function Widget(props)
                     <OverlayTrigger trigger="click" placement="right" overlay={popinfos}>
                         <Button variant="info" className="Widget-button">Parameters</Button>
                     </OverlayTrigger>
-                    <Button variant="success" className="Widget-button Widget-add">Add </Button>
+                    <Button variant="success" className="Widget-button Widget-add" onClick={handleShow}>Add </Button>
+                    <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>{props.name}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body><input type="text" name="name" placeholder={props.params} onChange={e => setName(e.target.value)}></input></Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
                 </div>
             </div>
         </React.Fragment>
